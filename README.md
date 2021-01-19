742028ea813cb8c6d9a405657d7ea8174960e4e8
Rivers Marc <m.t.rivers@hotmail.com>
2021-01-18 18:40:25 -0600

    Added README

---
+++ README.md
@@ -0,0 +1,35 @@
+UI Element Project
+
+Mini-project 3
+
+## Getting Started
+
+All files can be downloaded from the link below.
+https://github.com/mtr6093/UI-Pattern-Project-2
+
+
+### Prerequisites
+
+None
+
+### Installing
+
+You can open the index.html file from the link above to se the web based UI.
+
+### Visuals
+
+The site will show ingrdients and instructions for making 4 different cocktails in an accordian styled UI. Just open an accordian to learn how to make your drink of choice.
+
+
+## Authors
+
+Marc Rivers
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+I would like to acknowledge Codeing Journey for the great instructions on making an accordian layout.
+
--- UI.js
+++ UI.js
@@ -20,16 +20,12 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 });
 
 
-// let b1 =document.getElementById("b1")
 
-// b1.addEventListener('click', clicked1)
-
-// let image = document.getElementsByClassName("randomImage")[0]
-// console.log(image)
 
 let b1Content = document.getElementById("c1")
 let b2Content = document.getElementById("c2")
-// console.log(b1Content)
+let b3Content = document.getElementById("c3")
+let b4Content = document.getElementById("c4")
 
 
 let mingredients = {
@@ -42,44 +38,90 @@ let dingredients = {
     type: ""
 }
 
+let pingredients = {
+    qty1: "",
+    type: ""
+}
+
+let singredients = {
+    qty1: "",
+    type: ""
+}
+
+
 let url = ' https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita' 
-    // console.log("working")
+    
     fetch(url)
-    .then(response => response.json())
-    .then(json => {
-    // console.log(json.drinks[0])
-    // console.log(json.drinks[0].strDrinkThumb)
-    // console.log(image)    
-    mingredients.type1 = json.drinks[0].strIngredient1
-    mingredients.qty1 = json.drinks[0].strMeasure1
-    mingredients.type2 = json.drinks[0].strIngredient2
-    mingredients.qty2 = json.drinks[0].strMeasure2
-    mingredients.type3 = json.drinks[0].strIngredient3
-    mingredients.qty3 = json.drinks[0].strMeasure3
-    mingredients.type4 = json.drinks[0].strIngredient4
-    mingredients.qty4 = json.drinks[0].strMeasure4
-    b1Content.textContent = json.drinks[0].strInstructions
-    list1()
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json)
+        mingredients.type1 = json.drinks[0].strIngredient1
+        mingredients.qty1 = json.drinks[0].strMeasure1
+        mingredients.type2 = json.drinks[0].strIngredient2
+        mingredients.qty2 = json.drinks[0].strMeasure2
+        mingredients.type3 = json.drinks[0].strIngredient3
+        mingredients.qty3 = json.drinks[0].strMeasure3
+        mingredients.type4 = json.drinks[0].strIngredient4
+        mingredients.qty4 = json.drinks[0].strMeasure4
+        b1Content.textContent = json.drinks[0].strInstructions
+        list1()
     })
 
     let url2 = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=dragonfly'
-    console.log("working")
+    
     fetch(url2)
-    .then(response => response.json())
-    .then(json => {
-    console.log(json.drinks[0])
-    // console.log(json.drinks[0].strDrinkThumb)
-    // console.log(image)    
-    dingredients.type1 = json.drinks[0].strIngredient1
-    dingredients.qty1 = json.drinks[0].strMeasure1
-    dingredients.type2 = json.drinks[0].strIngredient2
-    dingredients.qty2 = json.drinks[0].strMeasure2
-    dingredients.type3 = json.drinks[0].strIngredient3
-    dingredients.qty3 = json.drinks[0].strMeasure3
-    b2Content.textContent = json.drinks[0].strInstructions
-    list2()
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json)
+        dingredients.type1 = json.drinks[0].strIngredient1
+        dingredients.qty1 = json.drinks[0].strMeasure1
+        dingredients.type2 = json.drinks[0].strIngredient2
+        dingredients.qty2 = json.drinks[0].strMeasure2
+        dingredients.type3 = json.drinks[0].strIngredient3
+        dingredients.qty3 = json.drinks[0].strMeasure3
+        b2Content.textContent = json.drinks[0].strInstructions
+        list2()
     })
-// console.log(mingredients)
+
+    let url3 = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Pi%C3%B1a%20Colada'
+    // console.log("working")
+    fetch(url3)
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json) 
+        pingredients.type1 = json.drinks[0].strIngredient1
+        pingredients.qty1 = json.drinks[0].strMeasure1
+        pingredients.type2 = json.drinks[0].strIngredient2
+        pingredients.qty2 = json.drinks[0].strMeasure2
+        pingredients.type3 = json.drinks[0].strIngredient3
+        pingredients.qty3 = json.drinks[0].strMeasure3
+        b3Content.textContent = json.drinks[0].strInstructions
+        list3()
+    })
+
+    let url4 = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=singapore%20sling' 
+  
+    fetch(url4)
+        .then(response => response.json())
+        .then(json => {       
+            // console.log(json)
+        singredients.type1 = json.drinks[0].strIngredient1
+        singredients.qty1 = json.drinks[0].strMeasure1
+        singredients.type2 = json.drinks[0].strIngredient2
+        singredients.qty2 = json.drinks[0].strMeasure2
+        singredients.type3 = json.drinks[0].strIngredient3
+        singredients.qty3 = json.drinks[0].strMeasure3
+        singredients.type4 = json.drinks[0].strIngredient4
+        singredients.qty4 = json.drinks[0].strMeasure4
+        singredients.type5 = json.drinks[0].strIngredient5
+        singredients.qty5 = json.drinks[0].strMeasure5
+        singredients.type6 = json.drinks[0].strIngredient6
+        singredients.qty6 = json.drinks[0].strMeasure6
+        b4Content.textContent = json.drinks[0].strInstructions
+        list4()
+    })
+    
+
 
 function list1 () {
     let ul=document.createElement('ul')
@@ -112,35 +154,43 @@ function list2 () {
         let li3=document.createElement('li')
         ul.appendChild(li3)
         li3.innerHTML += dingredients.qty3 + dingredients.type3
-        
-        
-  
 }
 
-
-
-
-
-
-
-
-
-
-
-
-    // function clicked1 (event) {
-//     event.preventDefault()
-//     let url = ' https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita' 
-//     console.log("working")
-//     fetch(url)
-//   .then(response => response.json())
-//   .then(json => {
-//     console.log(json.drinks[0].strMeasure1)
-//     // console.log(json.drinks[0].strDrinkThumb)
-//     // console.log(image)
-//     b1Content.textContent = json.drinks[0].strMeasure1
-//     console.log(b1Content)
+function list3 () {
+    let ul=document.createElement('ul')
+    document.getElementById('c3').appendChild(ul)
     
-//   }  
-//   )
-// }
+        let li1=document.createElement('li')
+        ul.appendChild(li1)
+        li1.innerHTML += pingredients.qty1 + pingredients.type1
+        let li2=document.createElement('li')
+        ul.appendChild(li2)
+        li2.innerHTML += pingredients.qty2 + pingredients.type2
+        let li3=document.createElement('li')
+        ul.appendChild(li3)
+        li3.innerHTML += pingredients.qty3 + pingredients.type3
+}
+function list4 () {
+    let ul=document.createElement('ul')
+    document.getElementById('c4').appendChild(ul)
+    
+        let li1=document.createElement('li')
+        ul.appendChild(li1)
+        li1.innerHTML += singredients.qty1 + singredients.type1
+        let li2=document.createElement('li')
+        ul.appendChild(li2)
+        li2.innerHTML += singredients.qty2 + singredients.type2
+        let li3=document.createElement('li')
+        ul.appendChild(li3)
+        li3.innerHTML += singredients.qty3 + singredients.type3
+        let li4=document.createElement('li')
+        ul.appendChild(li4)
+        li4.innerHTML += singredients.qty4 + singredients.type4
+        let li5=document.createElement('li')
+        ul.appendChild(li5)
+        li5.innerHTML += singredients.type5
+        let li6=document.createElement('li')
+        ul.appendChild(li6)
+        li6.innerHTML += singredients.type6
+    }   
+
